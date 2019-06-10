e4968b06a81e45251214dea3232bd1f88992610a
Dadduuu <51355907+Dadduuu@users.noreply.github.com>
2019-06-10 12:30:54 +0530

    Update avg.java

--- avg.java
+++ avg.java
@@ -1,33 +1,23 @@
 import java.util.Scanner;
-class avg
+class even
 {
-float w1;
-float w2;
-int q1;
-int q2;
-
+int a;
 	void getdata()
 	{
 		Scanner s=new Scanner(System.in);
-		System.out.println("enter weight of first item");
-		w1=s.nextInt();
-		System.out.println("enter quantity of first item");
-		q1=s.nextInt();
+		System.out.println("enter number");
+		a=s.nextInt();
 		
-		System.out.println("enter weight of second item");
-		w2=s.nextInt();
-		System.out.println("enter quantity of second item");
-		q2=s.nextInt();
 	}
-	void average()
-	{
-	float avge=((w1*q1)+(w2*q2))/(q1+q2);
-	System.out.println("average= "+avge);
-	}
+
 public static void main(String arr[])
 {
-avg o=new avg();
-o.getdata();
-o.average();
+even e=new even();
+e.getdata();
+if(e.a%2==0)
+System.out.println("number is even");
+else 
+System.out.println("number is odd");
 }
 }
+
